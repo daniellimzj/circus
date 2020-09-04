43de1bbd3e08085a5098a9c547310f82d810ca62
daniellimzj <daniellimzijie@gmail.com>
2020-09-04 17:24:05 +0800

    Add lecture changes

--- src/main/java/circus/Trainer.java
+++ src/main/java/circus/Trainer.java
@@ -11,9 +11,9 @@ public class Trainer {
 
         Duck d2 = (Duck) a; // downcasting
         train(new Duck());
-        // train(new Parrot());
-        Animal a2 = new Animal();
-        Bird b2 = new Bird();
+        train(new Parrot());
+        // Animal a2 = new Animal();
+        // Bird b2 = new Bird();
     }
 
     private static void getToSpeak(Animal animal) {
@@ -21,7 +21,9 @@ public class Trainer {
     }
 
     private static void train(Bird bird) {
-        Duck d = (Duck) bird;
-        d.swim();
+        if (bird instanceof Duck) {
+            Duck d = (Duck) bird;
+            d.swim();
+        }
     }
 }
